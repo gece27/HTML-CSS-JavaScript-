8962e100cd0df00c71a6bb79218e83cdd4660f69
gece27 <86601511+gece27@users.noreply.github.com>
2023-08-14 14:58:15 +0300

    Create scripts.js

---
+++ Hafıza Oyunu (Memory Game)/scripts.js
@@ -0,0 +1,76 @@
+const cards = document.querySelectorAll('.memory-card');
+
+let hasFlippedCard = false;
+let lockBoard = false;
+let firstCard, secondCard;
+var score=0;
+var step=0;
+
+function flipCard() {
+  if (lockBoard) return;
+  if (this === firstCard) return;
+
+  this.classList.add('turn');
+
+  
+
+  if (!hasFlippedCard) {
+    hasFlippedCard = true;
+    firstCard = this;
+
+    return;
+  }
+
+  secondCard = this;
+  checkForMatch();
+}
+
+function checkForMatch() {
+  let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
+
+  //step part
+  step+=1
+  document.getElementById("step").innerHTML="step👉"+step
+
+  //score part
+  if(isMatch==true){
+  score+=1
+  var scorelabel=document.getElementById("score")
+  scorelabel.innerHTML="score👉"+score;
+  }
+  
+
+  isMatch ? disableCards() : unflipCards();
+}
+
+function disableCards() {
+  firstCard.removeEventListener('click', flipCard);
+  secondCard.removeEventListener('click', flipCard);
+
+  resetBoard();
+}
+
+function unflipCards() {
+  lockBoard = true;
+
+  setTimeout(() => {
+    firstCard.classList.remove('turn');
+    secondCard.classList.remove('turn');
+
+    resetBoard();
+  }, 1500);
+}
+
+function resetBoard() {
+  [hasFlippedCard, lockBoard] = [false, false];
+  [firstCard, secondCard] = [null, null];
+}
+
+(function shuffle() {
+  cards.forEach(card => {
+    let randomPos = Math.floor(Math.random() * 12);
+    card.style.order = randomPos;
+  });
+})();
+
+cards.forEach(card => card.addEventListener('click', flipCard));
